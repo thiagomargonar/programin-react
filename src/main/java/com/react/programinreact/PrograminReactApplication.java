bbb7a1c06e310780dcf9e7baa2f5668a40752f96
thiagomargonar <thiago.margonar@hotmail.com>
2022-09-28 13:21:50 -0300

    Initial commit

---
+++ src/main/java/com/react/programinreact/PrograminReactApplication.java
@@ -0,0 +1,13 @@
+package com.react.programinreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrograminReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrograminReactApplication.class, args);
+	}
+
+}
