2cacd5f99f89cb31d99ce4e4a4d003912dcbf99f
thiagomargonar <thiago.margonar@hotmail.com>
2022-09-30 00:11:02 -0300

    save

--- src/test/java/com/react/programinreact/test/FluxTest.java
+++ src/test/java/com/react/programinreact/test/FluxTest.java
@@ -2,6 +2,9 @@ package com.react.programinreact.test;
 
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -29,6 +32,106 @@ public class FluxTest {
 
         StepVerifier.create(fluxInt)
                 .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    void flushSubscriberFromList() {
+
+        Flux<Integer> fluxInt = Flux.fromIterable(List.of(1, 2, 3, 4))
+                .log();
+
+        fluxInt.subscribe(integer -> System.out.println(integer));
+
+        StepVerifier.create(fluxInt)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void flushSubscriberFromErros() {
+
+        Flux<Integer> fluxInt = Flux.range(1, 10)
+                .map(integer -> {
+                    if (integer == 4) {
+                        throw new IndexOutOfBoundsException();
+                    }
+                    return integer;
+                });
+
+        StepVerifier.create(fluxInt)
+                .expectNext(1, 2, 3)
+                .expectError(IndexOutOfBoundsException.class)
+                .verify();
+    }
+
+    @Test
+    void flushSubscriberFromUglyBlackPressure() {
+
+        Flux<Integer> flux = Flux.range(1, 10).log();
+
+        flux.subscribe(new Subscriber<Integer>() {
+            private Subscription subscription;
+            private int requestCount = 0;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                subscription.request(2);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                requestCount++;
+                if (requestCount >= 2) {
+                    requestCount = 0;
+                    subscription.request(2);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .verifyComplete();
+    }
+
+    @Test
+    void flushSubscriberFromNotSoUglyBlackPressure() {
+
+        Flux<Integer> flux = Flux.range(1, 10).log();
+        flux.subscribe(new BaseSubscriber<Integer>() {
+
+            int requestCount=2;
+            int count=0;
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(requestCount);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                count++;
+                if (count >= requestCount) {
+                    count = 0;
+                    request(requestCount);
+                }
+            }
+        });
+
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
                 .verifyComplete();
     }
 }
